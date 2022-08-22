79e32c2133aac8a32c9bb2848f803e8f0f5d8ab6
niadi <89728697+niadi@users.noreply.github.com>
2022-08-22 16:10:01 +0700

    Update mntl-app.js

--- mntl-app.js
+++ mntl-app.js
@@ -1,19 +1,4 @@
 /**
-* ViewImage gzip compressed to just 2K, small and superb native Native JavaScript lightbox plugin
-*
-* @name ViewImage.js
-* @version 2.0.0
-* @author Tokinx
-* @license MIT License - http://www.opensource.org/licenses/mit-license.php
-*
-* For usage and examples, visit:
-* https://tokinx.github.io/ViewImage/
-*
-* Copyright (c) 2017, biji.io
-*/
-window.ViewImage=new function(){this.target="[view-image] img",this.listener=e=>{if(e.ctrlKey||e.metaKey||e.shiftKey||e.altKey)return;const t=String(this.target.split(",").map((e=>`${e.trim()}:not([no-view])`))),i=e.target.closest(t);if(!i)return;const a=[...(i.closest("[view-image]")||document.body).querySelectorAll(t)].map((e=>e.href||e.src));this.display(a,i.href||i.src),e.stopPropagation(),e.preventDefault()},this.init=e=>{e&&(this.target=e),["removeEventListener","addEventListener"].forEach((e=>{document[e]("click",this.listener,!1)}))},this.display=(e,t)=>{let i=e.indexOf(t);const a=(new DOMParser).parseFromString(`\n \t\t\t\t<div class="view-image">\n \t\t\t\t<style>.view-image{position:fixed;inset:0;z-index:5000;padding:3%;display:flex;flex-direction:column;animation:view-image-in 300ms;backdrop-filter:blur(20px);-webkit-backdrop-filter:blur(20px)}.view-image__out{animation:view-image-out 300ms}@keyframes view-image-in{0%{opacity:0}}@keyframes view-image-out{100%{opacity:0}}.view-image-btn{width:32px;height:32px;display:flex;justify-content:center;align-items:center;cursor:pointer;border-radius:3px;background-color:rgba(255,255,255,0.2)}.view-image-btn:hover{background-color:rgba(255,255,255,0.5)}.view-image-close__full{position:absolute;inset:0;background-color:rgba(48,55,66,0.3);z-index:unset;cursor:zoom-out;margin:0}.view-image-container{height:0;flex:1;display:flex;align-items:center;justify-content:center;padding-bottom:3%}.view-image-lead{display:contents}.view-image-lead img{position:relative;z-index:1;max-width:100%;max-height:100%;object-fit:contain;border-radius:3px}.view-image-lead__in img{animation:view-image-lead-in 300ms}.view-image-lead__out img{animation:view-image-lead-out 300ms forwards}@keyframes view-image-lead-in{0%{opacity:0;transform:translateY(-20px)}}@keyframes view-image-lead-out{100%{opacity:0;transform:translateY(20px)}}[class*=__out] ~ .view-image-loading{display:block}.view-image-loading{position:absolute;inset:50%;width:8rem;height:2rem;color:#aab2bd;overflow:hidden;text-align:center;margin:-1rem -4rem;z-index:1;display:none}.view-image-loading::after{content:"";position:absolute;inset:50% 0;width:100%;height:3px;background:rgba(255,255,255,0.5);transform:translateX(-100%) translateY(-50%);animation:view-image-loading 800ms -100ms ease-in-out infinite}@keyframes view-image-loading{0%{transform:translateX(-100%)}100%{transform:translateX(100%)}}.view-image-tools{position:relative;display:flex;justify-content:space-between;align-content:center;color:#fff;max-width:600px;width:100%;margin:0 auto;padding:10px;border-radius:5px;background:rgb(20 20 20 / 35%);margin-bottom:constant(safe-area-inset-bottom);margin-bottom:env(safe-area-inset-bottom);z-index:1}.view-image-tools__count{width:60px;display:flex;align-items:center;justify-content:center}.view-image-tools__flip{display:flex;gap:10px}.view-image-tools [class*=-close]{margin:0 10px}</style>\n \t\t\t\t<div class="view-image-container">\n \t\t\t\t<div class="view-image-lead"></div>\n \t\t\t\t<div class="view-image-loading"></div>\n \t\t\t\t<div class="view-image-close view-image-close__full"></div>\n \t\t\t\t</div>\n \t\t\t\t<div class="view-image-tools">\n \t\t\t\t<div class="view-image-tools__count">\n \t\t\t\t<span><b class="view-image-index">${i+1}</b>/${e.length}</span>\n \t\t\t\t</div>\n \t\t\t\t<div class="view-image-tools__flip">\n \t\t\t\t<div class="view-image-btn view-image-tools__flip-prev">\n \t\t\t\t<svg width="20" height="20" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M31 36L19 24L31 12" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>\n \t\t\t\t</div>\n \t\t\t\t<div class="view-image-btn view-image-tools__flip-next">\n \t\t\t\t<svg width="20" height="20" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M19 12L31 24L19 36" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>\n \t\t\t\t</div>\n \t\t\t\t</div>\n \t\t\t\t<div class="view-image-btn view-image-close">\n \t\t\t\t<svg width="16" height="16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M8 8L40 40" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path d="M8 40L40 8" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>\n \t\t\t\t</div>\n \t\t\t\t</div>\n \t\t\t\t</div>\n \t\t\t\t`,"text/html").body.firstChild,n=function(e){const t={Escape:"close",ArrowLeft:"tools__flip-prev",ArrowRight:"tools__flip-next"};t[e.key]&&a.querySelector(`.view-image-${t[e.key]}`).click()},o=e=>{const t=new Image,i=a.querySelector(".view-image-lead");i.className="view-image-lead view-image-lead__out",setTimeout((()=>{i.innerHTML="",t.onload=function(){setTimeout((()=>{i.innerHTML=`<img src="${t.src}" alt="ViewImage" no-view/>`,i.className="view-image-lead view-image-lead__in"}),100)},t.src=e}),300)};document.body.appendChild(a),o(t),window.addEventListener("keydown",n),a.onclick=t=>{t.target.closest(".view-image-close")?(window.removeEventListener("keydown",n),a.onclick=null,a.classList.add("view-image__out"),setTimeout((()=>a.remove()),290)):t.target.closest(".view-image-tools__flip")&&(i=t.target.closest(".view-image-tools__flip-prev")?0===i?e.length-1:i-1:i===e.length-1?0:i+1,o(e[i]),a.querySelector(".view-image-index").innerHTML=i+1)}}};
-
-/**
 * 
 * Main Script
 * 
@@ -32,86 +17,43 @@ document.addEventListener("turbo:load", function() {
 		window.location.href = window.location.href.replace("?m=1", "?m=0");
 	}					
 
-	/* get page_type */
-	is_page = $("meta[name='is_page']").attr('content');			
+	/* load carousel */
+	carouselIndex();
 
-	/* load script global */			
-	mainNav(); /* load Nav */
-	darkMode(); /* load darkMode */
-	backToTop(); /* load back to top */			
+	/* load Nav */
+	mainNav();
 
-	/* load script if on page multiple */
-	if (is_page == 'multiple') {
+	/* load darkMode */
+	darkMode();			
 
-		/* load carousel */
-		carouselIndex();
+	/* load postPaginationMultiple */
+	postPaginationMultiple();
 
-		/* load postPaginationMultiple */
-		let postIndex = $("#postPaginationMultiple"), postIndexHasLoaded = 0;
-		if (postIndex.length > 0) {	
+	/* Load adsMove */
+	adsMove();
 
-			/* first remove function prevent duplciate call */
-			delete window.postPaginationMultiple;
+	/* load back to top */
+	backToTop();
 
-			/* on load and */
-			if(($(window).scrollTop() + $(window).height()) >= ($(document).height() - 200) && !postIndexHasLoaded) {
+	/* load after scroll on post-detail */
+	let postDetail = $(".post-detail"), postDetailHasLoaded = 0;
+	if (postDetail.length > 0) {	
+		$(window).on("load scroll", function() {
+			var height = postDetail.outerHeight();
+			if(($(window).scrollTop() + $(window).height()) >= height && !postDetailHasLoaded) {
 
-				/* set postIndexHasLoaded */
-				postIndexHasLoaded = 1;
+				/* set postDetailHasLoaded */
+				postDetailHasLoaded = 1;
 
-				postPaginationMultiple(function(){
-					/* set postIndexHasLoaded */
-					postIndexHasLoaded = 0;
-				});							
+				/* load recentPost */
+				relatedPost();
+
+				/* load commentPost */
+				postComment();
+
 			}
-
-			/* on scroll */
-			$(window).on("scroll", function() {
-				if(($(window).scrollTop() + $(window).height()) >= ($(document).height() - 200) && !postIndexHasLoaded) {
-
-					/* set postIndexHasLoaded */
-					postIndexHasLoaded = 1;
-
-					postPaginationMultiple(function(){
-						/* set postIndexHasLoaded */
-						postIndexHasLoaded = 0;
-					});							
-				}
-			});		
-		}
-
-		/* Load adsMoveFeed */
-		adsMoveFeed();		
+		});						
 	}
-
-	if (is_page == 'single') {
-
-		/* Call viewImage */
-		window.ViewImage && ViewImage.init('.post-content img, .post-content a img');
-
-		/* Load adsMovePost */
-		adsMovePost();		
-
-		/* load after scroll on post-detail */
-		let postDetail = $(".post-detail"), postDetailHasLoaded = 0;
-		if (postDetail.length > 0) {	
-			$(window).on("load scroll", function() {
-				var height = postDetail.outerHeight();
-				if(($(window).scrollTop() + $(window).height()) >= height && !postDetailHasLoaded) {
-
-					/* set postDetailHasLoaded */
-					postDetailHasLoaded = 1;
-
-					/* load recentPost */
-					relatedPost();
-
-					/* load commentPost */
-					postComment();
-
-				}
-			});						
-		}					
-	}		
 
 });	
 
@@ -129,10 +71,10 @@ function mainNav()
 		selection = menu.data('nav');
 
 		/* remove class active global */
-		$("#main-nav div.nav-link").removeClass('active fw-bold');
+		$("#main-nav div.nav-link").removeClass('active');
 
 		/* add class active current selection */
-		menu.addClass('active fw-bold');
+		menu.addClass('active');
 
 		/* hide element not selection */
 
@@ -151,7 +93,7 @@ function mainNav()
 		$("#main-categories, #main-archive, #main-sidebar").addClass('d-none');
 
 		/* remove class active */
-		$("#main-nav div.nav-link").removeClass('active fw-bold');
+		$("#main-nav div.nav-link").removeClass('active');
 
 		/* show header and main content */
 		$("#widget-main-header, #widget-main-pinned, #main-content").removeClass('d-none');
@@ -196,53 +138,55 @@ function carouselIndex(){
 }
 
 /* post index pagination */
-function postPaginationMultiple(callback)
+function postPaginationMultiple()
 {
+	let postIndex = $("#postPaginationMultiple"), postIndexHasLoaded = 0;
+	if (postIndex.length > 0) {	
+		$(window).on("load scroll", function() {
+			if(($(window).scrollTop() + $(window).height()) >= ($(document).height() - 100) && !postIndexHasLoaded) {
 
-	/* get and check pagination link */
-	var postPaginationMultipleLink = $("#postPaginationMultiple a.pagination-link"),
-	postPaginationMultipleLinkUrl = postPaginationMultipleLink.attr('href'),
-	postPaginationMultipleLinkLoading = postPaginationMultipleLink.data('loading');
+				/* set postIndexHasLoaded */
+				postIndexHasLoaded = 1;
 
-	if (postPaginationMultipleLinkUrl !== undefined) {
+				/* get and check pagination link */
+				var postPaginationMultipleLink = $("#postPaginationMultiple a.pagination-link"),
+				postPaginationMultipleLinkUrl = postPaginationMultipleLink.attr('href'),
+				postPaginationMultipleLinkLoading = postPaginationMultipleLink.data('loading');
 
-		/* send animation */
-		$("#postPaginationMultiple").html(`<div class="text-center py-3"><div class="spinner-border text-primary" role="status"><span class="visually-hidden">${postPaginationMultipleLinkLoading}</span></div></div>`);
+				if (postPaginationMultipleLinkUrl !== undefined) {
 
-		$.ajax({
-			url: postPaginationMultipleLinkUrl,
-			dataType: 'html',
-			success: function(output) {												
+					/* send animation */
+					$("#postPaginationMultiple").html(`<div class="text-center py-3"><div class="spinner-border text-primary" role="status">
+						<span class="visually-hidden">${postPaginationMultipleLinkLoading}</span>
+						</div></div>`);
 
-				/* must have delay for prevent fast call again function */
-				setTimeout(function() {
+					$.ajax({
+						url: postPaginationMultipleLinkUrl,
+						dataType: 'html',
+						success: function(output) {												
 
-					/* grab pagination */
-					let paging_link = $(output).find('#postPaginationMultiple a.pagination-link');
+							setTimeout(function() {
+								/* replace pagination with new link */
+								$("#postPaginationMultiple").html($(output).find('#postPaginationMultiple a.pagination-link').clone());
 
-					/* replace pagination with new link */
-					$("#postPaginationMultiple").html(paging_link.clone());
+								/* append new posts */
+								$('#postIndex').append($('#postIndex', $(output).wrap("<div/>")).html()); 
 
-					/* append new posts */
-					$('#postIndex').append($('#postIndex', $(output).wrap("<div/>")).html()); 
+								/* ads feed */
+								let adsFeed = $(output).find('.ads-move-to-feed');          
+								$(".ads-target-feed:last").append(adsFeed);
 
-					/* ads feed */
-					let adsFeed = $(output).find('.ads-move-to-feed');          
-					$(".ads-target-feed:last").append(adsFeed);
+								/* set postIndexHasLoaded */
+								postIndexHasLoaded = 0;
+							}, 1000);
+						}
+					})
+				}else{
+					$("#postPaginationMultiple").html(`<div class="text-center text-success fw-bold fs-3"><i class="bi bi-check-circle"></i></div>`);
+				}
 
-					/* check if paging not exist */
-					if (paging_link.length > 0) {
-
-						/* run callback */
-						callback();
-
-					}else{
-						$("#postPaginationMultiple").html(`<div class="text-center text-success fw-bold fs-3"><i class="bi bi-check-circle"></i></div>`);
-					}
-
-				}, 1000);
 			}
-		})
+		});		
 	}
 }
 
@@ -252,8 +196,7 @@ function relatedPost()
 	const relatedPost = $("#relatedPost"),
 	recentPostLimit = relatedPost.data('limit'),				
 	recentPostId = relatedPost.data('id'),			
-	relatedPostTitle = relatedPost.data('title'),
-	relatedPostLang = relatedPost.data('lang');			
+	relatedPostTitle = relatedPost.data('title');
 
 	/* check if have relatedPost element */
 	if(relatedPost.length > 0) {
@@ -302,19 +245,15 @@ function relatedPost()
 					data.id = post.id.$t.substr(post.id.$t.indexOf("post-") + 5);
 					data.title = post.title.$t;
 
+
 					/* get date as format */
 					const dateFormat =  data => {
-						let format_moth;
-						if (relatedPostLang == 'id') {
-							format_moth = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September", "Oktober", "November", "Desember"];
-						}else{									
-							format_moth = ["January","February","March","April","May","June","July","August","September", "October", "November", "December"];
-						}
+						let format_moth = ["January","February","March","April","May","June","July","August","September", "October", "November", "December"];
 						let date = new Date(data),
 						day = date.getDate(),
 						month = format_moth[date.getMonth()],
 						year = date.getFullYear();
-						return `${day} ${month} ${year}`;
+						return `${month} ${day}, ${year}`;
 					};
 
 					data.published = dateFormat(post.published.$t);
@@ -331,7 +270,7 @@ function relatedPost()
 					if (post.media$thumbnail) {
 						data.image = post.media$thumbnail.url.replace("/s72-c/","/w150/");
 					}else{
-						data.image = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjhlzrDBL6kAIclFYFeRx7cF3w-X1qqkcjEP_io-XVCIPhWq0edt2PCZFd0-0XlZANlmpk_UkS3svsmZVthApDAx5PWJM9xq1ebAsg4dhuf-izxG48jxSpfe6NLCD5ylvThifZl0_NQeNYxlv3r4YppLhiOEFTkXAe1LfjbBaJ498m6-1DSVvJ6mlyKEA/s300/Screenshot_2018-02-13-15-42-38.jpg';
+						data.image = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj3Ia32aeortEtnG6libJiXjj6It8o5ikSjQEaPSvLK09ORfnWovo0FQX25VeAB-cSySlozaol3LZ-9C7BVpfeMMcxtEE7SxW-BCLVX4wwywof7kiBCp8nsHbre7ScleqehuZzY0G17M1mBV1H-KzYd1QW098SZzEAHE0GecLG5u2ULhnEYtKTsWUMp/w300/no-image.jpg';
 					}
 
 					posts.push(data);
@@ -438,15 +377,13 @@ function postComment()
 }	
 
 /* ads move */
-function adsMovePost()
+function adsMove()
 {
 	let countTargetMiddle = $(`.post-content p`).length, middleElement = parseInt(countTargetMiddle / 2);;
 	$('.ads-move-to-post-middle').insertAfter(`.post-content p:nth-of-type(${middleElement})`);
-	$(".ads-move-to-post-bottom").appendTo(".ads-target-post-bottom");
-}
 
-function adsMoveFeed()
-{
+	$(".ads-move-to-post-bottom").appendTo(".ads-target-post-bottom");
+
 	$(".ads-move-to-feed").appendTo(".ads-target-feed");
 }
 
@@ -482,4 +419,4 @@ function copyThis(data) {
 	$("#gocopy").remove();
 	/* show message */
 	showToast('Success Copy');
-}		  
+}
